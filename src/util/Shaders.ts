c1510149e0e9b29b671b86f15e8980532f43a05e
Dennis Ego <dennis.ego@artcoresociety.com>
2023-08-10 13:41:30 +0200

    use pos in shaders

--- src/util/Shaders.ts
+++ src/util/Shaders.ts
@@ -1,9 +1,11 @@
 export function getVertexShader() {
     return `
+        varying vec3 pos;
         uniform float u_time;
         
         void main() {
             vec4 result;
+            pos = position;
             
             result = vec4(position.x, 2.0*sin(position.z/4.0 + u_time) + position.y, position.z, 1.0);
             
@@ -16,10 +18,15 @@ export function getVertexShader() {
 
 export function getFragmentShader() {
     return `
+        varying vec3 pos;
         uniform float u_time;
          
         void main() {
-            gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
+            if(pos.x >= 0.0) {
+                 gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
+            } else {
+                gl_FragColor = vec4(0.0, abs(cos(u_time)), 0.0, 1.0);
+            }
         }
     `;
 }
